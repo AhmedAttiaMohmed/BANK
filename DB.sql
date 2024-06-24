cbc49a055b03ee4ef7492eb395ea90d7a37183d9
AhmedAttiaMohmed <abkr8266@gmail.com>
2024-06-24 13:35:14 +0300

    Second commit

--- DB.sql
+++ DB.sql
@@ -8,14 +8,36 @@ create table if not exists users(
     password varchar(255) not null,
     address varchar(255) not null,
     vic_number int(255) not null unique,
+    role enum('users','salesman') default 'users',
     created_at timestamp default current_timestamp,
     updated_at timestamp default current_timestamp
 );
 
-create table if not exists salesman(
+create table if not exists posts(
 	id int primary key auto_increment,
-    name varchar(255) not null,
-    email varchar(255) not null unique,
-	password varchar(255) not null,
-	
-	
+    title varchar(255) not null,
+    content text,
+	image varchar(255),
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp,
+	user_id int,
+	constraint fk_posts_users
+    foreign key (user_id)
+    references users(id)
+
+);
+
+create table if not exists comments(
+	id int primary key auto_increment,
+    comment varchar(255) not null,
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp,
+    user_id int,
+    post_id int,
+    constraint fk_posts_users_user_id
+    foreign key (user_id)
+    references users(id),
+    constraint fk_posts_comments
+    foreign key (user_id)
+    references users(id)
+);
